640b96fdcb4488a0947bf4bbd9c0a2fe049a0561
akash <akash.aher@mediassist.in>
2021-04-16 10:48:51 +0530

    Initial commit

---
+++ util/src/main/java/com/example/util/ImageUtil.kt
@@ -0,0 +1,9 @@
+package com.example.util
+
+/**
+ * Created by akash on 16,04,2021
+ */
+
+ fun getMediBuddyUrl():String{
+    return "https://media-exp1.licdn.com/dms/image/C4D0BAQE3jmY8hHhHfg/company-logo_200_200/0/1614597825264?e=2159024400&v=beta&t=cWet1ebN1wRL8cVb4UjnAAk47M2AgCjUC-UZUONkOfY"
+}
